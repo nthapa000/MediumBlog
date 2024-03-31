ca041d112c89219cc3a708fce318f420fa1ce833
nthapa000 <nishantthapa0000@gmail.com>
2024-04-01 01:22:19 +0530

    Initialized_Routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/v1/signup', (c) => {
+  return c.text('Sign up!')
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('Sign in!')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('post blog!')
+})
+app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+// dynamic parameter
 
 export default app
