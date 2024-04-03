6470f2993665b3e031a7fe11db331948f74973bb
nthapa000 <nishantthapa0000@gmail.com>
2024-04-03 15:21:37 +0530

    Signin/Signup routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,61 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign} from 'hono/jwt'
 
+const app = new Hono<{
+    Bindings: {
+      DATABASE_URL: string,
+      JWT_SECRET:string
+    }
+}>()
 
-const app = new Hono()
-
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup',async (c) => {
+  // c is basically context has all our request and response and environment variable etc
+  // specify our env variable type
+  // we can't give environment variable outside 
+  // we don't have global access in serverless
   const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
+    datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
+// .env  accessed from the wrangler.toml
+// Cloudflare picks environment variable from wrangler.toml
 
-  return c.text('Sign up!')
+// this is how we get the body in Hono
+// we have to await when it is converted to json
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.email
+    },
+  })
+  const token =await sign({id:user.id},c.env.JWT_SECRET)
+  return c.json({jwt: token})
 })
-app.post('/api/v1/signin', (c) => {
-  return c.text('Sign in!')
+
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
+      password: body.password
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
 })
+
 app.post('/api/v1/blog', (c) => {
   return c.text('post blog!')
 })
