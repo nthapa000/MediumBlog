39d8217d8f07769c4052ad1f975e013f03ddb4c4
nthapa000 <nishantthapa0000@gmail.com>
2024-04-04 17:30:20 +0530

    Blog Routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,23 +1,91 @@
+import { PrismaClient } from "@prisma/client/edge";
 import { Hono } from "hono";
+import { withAccelerate } from "@prisma/extension-accelerate";
 export const blogRouter = new Hono<{
-    // passing the generics 
-      Bindings: {
-        DATABASE_URL: string;
-        JWT_SECRET:string;
-      }
-  }>();
+  // passing the generics
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-blogRouter.use('/api/v1/blog/*', async (c, next) => {
-    await next()
+blogRouter.use("/*", async (c, next) => {
+  await next();
+});
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: "1",
+      // the extraction will occur in the middleware
+    },
+  });
+
+  return c.json({
+    id: blog.id,
+  });
+});
+
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.post.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+      // the extraction will occur in the middleware
+    },
+  });
+
+  return c.json({
+    id: blog.id,
+  });
+});
+
+blogRouter.get("/:id", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    // must be wrapped because this request can fail due to multiple reasons
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: body.id,
+      },
+    });
+    return c.json({
+      blog,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.json({
+      message: "Error while fetching blog post",
+    });
+  }
+});
+
+// Ideally we should add pagination meaning that we should not add all the post , suppose we return only 10 and user ask for more when it scroll
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const blogs = prisma.post.findMany();
+  //getting all the blogs
+  return c.json({
+    blogs
   })
-  
-  blogRouter.post('/api/v1/blog', (c) => {
-    return c.text('post blog!')
-  })
-  blogRouter.put('/api/v1/blog', (c) => {
-    return c.text('Hello Hono!')
-  })
-  blogRouter.get('/api/v1/blog/:id', (c) => {
-    return c.text('Hello Hono!')
-  })
-  
+});
